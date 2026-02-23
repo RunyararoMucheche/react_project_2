aff285b041a8dce7d756c2a3003308e7882844a9
RunyararoMucheche <your_email@example.com>
2026-02-23 20:03:17 +0200

    Make edits to App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import entries from "./data.js";
 export default function App() {
   // Creating Entry elements
   const entryElement = entries.map((entry) => {
-    console.log(entry);
-    return <Entry key={entry.id} entry={entry} />;
+    return <Entry key={entry.id} {...entry} />;
   });
 
   // Returning the react data
